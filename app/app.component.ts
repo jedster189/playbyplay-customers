569e3f5de78bf6cffa282c9d978d0181621a68d5
Jared Egbert <jared.egbert@hotmail.com>
2016-07-13 16:05:28 -0600

    Databinding stuff

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
     // [] means property binding
     // () mean event binding  
+    // [()] 
 
     title='Customer App';
     name='Jared';
